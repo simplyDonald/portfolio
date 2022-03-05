4cc1867b899bcf0d1ebda9d46257375350fdc5a2
simplyDonald <donaldabuah@gmail.com>
2022-03-04 20:22:12 -0500

    add components folder

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Your portfolio begins here bro</h2>
+    <div className="">
+      {/* <h2>Your portfolio begins here bro</h2>
+      <p>Flintstones</p> */}
+      <img className="w-full h-auto" src="Photo by natalie Herregods on Unsplash.jpg" alt="" />
     </div>
   );
 }
