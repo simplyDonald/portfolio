4a2a53fdee851e0e3dc105b63889006d261e811e
simplyDonald <donaldabuah@gmail.com>
2022-03-21 16:45:01 -0400

    edit styling for job title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,12 +12,14 @@ function Hero() {
           Donald
           <span className="text-[#FFD700] y pl-0 font-sans"> Abuah.</span>{" "}
         </p>
-        <p className="text-white text-3xl pl-2 font-body">FullStack Software Developer</p>
+        <p className="text-white text-3xl pl-2 font-body">
+          <span className="italic">FullStack</span> <span className="font-semibold"> Software Developer</span> 
+        </p>
         <p className="text-[#d8f3dc] text-base pl-2 leading-6 max-w-lg mt-3 font-body">
           I’m a software developer specializing in creating easy to use web
-          experiences with intercative and appealing design. 
+          experiences with engaging and appealing designs.
         </p>
-        <blockquote className="text-[#d8f3dc] mt-3">
+        <blockquote className="text-[#d8f3dc] mt-3 max-w-xs">
           ...the universe stands aside for those who know where they are going
         </blockquote>
       </div>
