c66db4e96cb03b5d8eb7d3706e6539796f7d6a6c
simplyDonald <donaldabuah@gmail.com>
2022-03-21 15:56:03 -0400

    refactor style with tailwind class

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
           <span className="text-[#FFD700] y pl-0 font-sans"> Abuah.</span>{" "}
         </p>
         <p className="text-white text-3xl pl-2 font-body">FullStack Software Developer</p>
-        <p className="text-[#d8f3dc] text-xl pl-2 msg1 mt-3 font-body">
+        <p className="text-[#d8f3dc] text-base pl-2 leading-6 max-w-lg mt-3 font-body">
           I’m a software developer specializing in creating easy to use web
           experiences with intercative and appealing design. 
         </p>
