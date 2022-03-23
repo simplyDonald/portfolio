b844d6101af61cc7a5831cdd8f532eef790839e4
simplyDonald <donaldabuah@gmail.com>
2022-03-23 16:00:51 -0400

    style about header

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 function About() {
-  return ( 
-    <div  id="about" className="text-white mx-auto flex justify-center py-44 my-6 max-w-4xl border border-red-600">
-      <p>About Donald</p>
-      <div className="ml-6">picture g0es here4</div>
+  return (
+    <div id="about">
+      <p className=" text-3xl m-8">About Me</p>
+      <div className="">picture g0es here4</div>
     </div>
   );
 }
