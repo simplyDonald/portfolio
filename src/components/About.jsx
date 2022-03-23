91f4adb3b6a1e117f48dc69559a4a77e86b2e5a0
simplyDonald <donaldabuah@gmail.com>
2022-03-23 17:05:09 -0400

    format header element

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div id="about">
-      <p className=" text-3xl m-8">About Me</p>
+      <h2 className="abt-heading text-3xl m-8">About Me</h2>
       <div className="">picture g0es here4</div>
     </div>
   );
