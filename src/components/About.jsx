ba5788e2beb7d4ea8addc1873ea19e2c2f427b8e
simplyDonald <donaldabuah@gmail.com>
2022-03-23 15:44:11 -0400

    centralise div sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 function About() {
   return ( 
-    <div  id="about" className="text-white">
+    <div  id="about" className="text-white mx-auto flex justify-center py-44 my-6 max-w-4xl border border-red-600">
       <p>About Donald</p>
+      <div className="ml-6">picture g0es here4</div>
     </div>
   );
 }
