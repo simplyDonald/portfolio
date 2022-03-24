d0ce782a28b4e91c7a81e8199c1167a6aa3f599a
simplyDonald <donaldabuah@gmail.com>
2022-03-24 14:42:06 -0400

    refactor stroke using tailwind properties

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,26 @@
 function About() {
   return (
     <div id="about">
-      <h2 className="abt-heading text-3xl m-8">About Me</h2>
-      <div className="">picture g0es here4</div>
+      <h2 className="stroke text-3xl m-8">
+        About Me
+      </h2>
+      <div className="">
+        <div>
+          Hello! My name is Donald and I enjoy creating things that live on the
+          internet. My interest in web development started back in 2012 when I
+          decided to try editing custom Tumblr themes — turns out hacking
+          together a custom reblog button taught me a lot about HTML & CSS!
+          Fast-forward to today, and I’ve had the privilege of working at an
+          advertising agency, a start-up, a huge corporation, and a student-led
+          design studio. My main focus these days is building accessible,
+          inclusive products and digital experiences . I also recently launched
+          a course that covers everything you need to build a web app with the
+          Spotify API using Node & React. Here are a few technologies I’ve been
+          working with recently: JavaScript (ES6+) TypeScript React Eleventy
+          Node.js WordPress
+        </div>
+        <div>image div</div>
+      </div>
     </div>
   );
 }
