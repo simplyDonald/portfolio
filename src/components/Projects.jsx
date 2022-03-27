6a55482e2aae4bf06d17cac093a096cd76d8650b
simplyDonald <donaldabuah@gmail.com>
2022-03-26 22:52:57 -0400

    refactpr dynamic project rendering

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,48 +10,86 @@ import { techStack } from "techStack";
 
     const projects = techStack.map((item)=>{
       return (
-        <div className="relative flex mb-4 h-96">
+        // <div key={item.name} className="relative flex mb-4 h-96">
+        //   <div className="w-8/12 h-80 text-center self-start">
+        //     <img
+        //       src={item.img}
+        //       alt={`Screenshot of ${item.name} page`}
+        //       className="fofo hover:filter-none transition ease-out delay-700 hover:cursor-pointer"
+        //     />
+
+        //     <div className="w-2/4 h-max absolute font-body right-14 grid grid-cols-1">
+        //       <h2 className=" mt-2 text-sm text-[#64FFDA] justify-self-end">
+        //         Featured Project
+        //       </h2>
+        //       <h3 className="text-xl justify-self-end text-[#FFD700]">
+        //         {item.name}
+        //       </h3>
+        //       <p className=" leading-4 m-5 bg-[#112240] text-left p-6">
+        //         {item.desc}
+        //       </p>
+        //       {/* continue here */}
+        //       <ul className=" py-6 justify-self-end text-xs text-[#8892B0] mr-4">
+        //         {item.stack.map((stk) => {
+        //           return (
+        //             <li key={stk} className=" inline-block mr-2">
+        //               {stk}
+        //             </li>
+        //           );
+        //         })}
+        //       </ul>
+        //       <a
+        //         href={item.github}
+        //         rel="noreferrer"
+        //         className="justify-self-end mr-4"
+        //         target="_blank"
+        //       >
+        //         <img
+        //           className="invert pb-1"
+        //           src="github-icon.png"
+        //           alt="Github icons created by Pixel perfect - Flaticon"
+        //           width="20"
+        //           heigth="20"
+        //         />
+        //       </a>
+        //     </div>
+        //   </div>
+        // </div>
+        <div key={item.name} className="relative flex mb-4 h-96">
+          
           <div className="w-8/12 h-80 text-center self-start">
             <img
               src={item.img}
-              alt={`Screenshot of ${item.name}`}
+              alt={`screenshot of ${item.name} page`}
               className="fofo hover:filter-none transition ease-out delay-700 hover:cursor-pointer"
             />
+          </div>
 
-            <div className="w-2/4 h-max absolute font-body right-14 grid grid-cols-1">
-              <h2 className=" mt-2 text-sm text-[#64FFDA] justify-self-end">
-                Featured Project
-              </h2>
-              <h3 className="text-xl justify-self-end text-[#FFD700]">
-                {item.name}
-              </h3>
-              <p className=" leading-4 m-5 bg-[#112240] text-left p-6">
-                {item.desc}
-              </p>
-              {/* continue here */}
-              <ul className=" py-6 justify-self-end text-xs text-[#8892B0] mr-4">
-                {item.stack.map((stk)=>{
-                  return <li className=" inline-block mr-2">{stk}</li>;
-                })}
-                {/* <li className=" inline-block mr-2">techStack1</li>
-                <li className=" inline-block">techStack2</li>
-                <li className=" inline-block">techStack3</li> */}
-              </ul>
-              <a
-                href={item.github}
-                rel="noreferrer"
-                className="justify-self-end mr-4"
-                target="_blank"
-              >
-                <img
-                  className="invert pb-1"
-                  src="github-icon.png"
-                  alt="Github icons created by Pixel perfect - Flaticon"
-                  width="20"
-                  heigth="20"
-                />
-              </a>
-            </div>
+          <div className="w-2/4 h-max absolute font-body right-14 grid grid-cols-1">
+            <h2 className=" mt-2 text-sm text-[#64FFDA] justify-self-end">
+              Featured Project
+            </h2>
+            <h3 className="text-xl justify-self-end text-[#FFD700]">{item.name}</h3>
+            <p className=" leading-4 m-5 bg-[#112240] text-left p-6">
+              {item.desc}
+            </p>
+            <ul className=" py-6 justify-self-end text-xs text-[#8892B0] mr-4">
+              {item.stack.map((stk)=> <li key={stk} className=" inline-block mr-2">{stk}</li>)}
+            </ul>
+            <a
+              href="https://github.com/simplyDonald/tweeter"
+              rel="noreferrer"
+              className="justify-self-end mr-4"
+              target="_blank"
+            >
+              <img
+                className="invert pb-1"
+                src="github-icon.png"
+                alt="Github icons created by Pixel perfect - Flaticon"
+                width="20"
+                heigth="20"
+              />
+            </a>
           </div>
         </div>
       );
--- src/techStack.js
+++ src/techStack.js
@@ -6,26 +6,26 @@ export const techStack =[
     github:"https://github.com/xihai01/Pandemic-Tracker",
     desc: "Pandemic tracker is an interactive map application built with React using d3.js and a Rails API backend that displays restrictions and key covid statistics for any region within the map.",
     img:"pandemic-tracker.gif"
+  },
+  {
+    name: "Interview Scheduler",
+    stack: ["React","SASS", "Axios", "Node.js", "Express", "PostgreSQL"],
+    github:"https://github.com/simplyDonald/scheduler",
+    desc: "A Web App for students to book appointments with Mentors. They can edit, create and cancel scheduled appointments.",
+    img: "scheduler.png"
+  },
+  {
+    name: "Smart To-Do List",
+    stack: ["HTML", "CSS", " Vanilla JS", "jQuery", "AJAX", "Node", "Express", "PostgresSQL", "Bootstrap"],
+    github:"https://github.com/simplyDonald/midterm-smartToDoList",
+    desc: "An application built with Node and Express that allows users to create to-do items and have them auto-categorized into four categories",
+    img: "smart-todo-list.png"
+  },
+  {
+    name: "Tweeter",
+    stack: ["JavaScript", "jQuery", "AJAX","Express", "Node"],
+    github:"https://github.com/simplyDonald/tweeter",
+    desc: "Tweeter is a simple, single-page Twitter clone.",
+    img: "tweeter.png"
   }
-  // {
-  //   name: "Interview Scheduler",
-  //   stack: ["React","SASS", "Axios", "Node.js", "Express", "PostgreSQL"],
-  //   github:"https://github.com/simplyDonald/scheduler",
-  //   desc: "A Web App for students to book appointments with Mentors. They can edit, create and cancel scheduled appointments.",
-  //   img: "scheduler.png"
-  // },
-  // {
-  //   name: "Smart To-Do List",
-  //   stack: ["HTML", "CSS", " Vanilla JS", "jQuery", "AJAX", "Node", "Express", "PostgresSQL", "Bootstrap"],
-  //   github:"https://github.com/simplyDonald/midterm-smartToDoList",
-  //   desc: "An application built with Node and Express that allows users to create to-do items and have them auto-categorized into four categories",
-  //   img: "smart-todo-list.png"
-  // },
-  // {
-  //   name: "Tweeter",
-  //   stack: ["JavaScript", "jQuery", "AJAX","Express", "Node"],
-  //   github:"https://github.com/simplyDonald/tweeter",
-  //   desc: "Tweeter is a simple, single-page Twitter clone.",
-  //   img: "tweeter.png"
-  // }
 ]
