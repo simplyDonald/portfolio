1b2ed75bdb35ccfc52cb1e3372416b0f72616103
simplyDonald <donaldabuah@gmail.com>
2022-03-27 00:45:54 -0400

    add margin auto to list items

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -16,9 +16,9 @@ const TechStack = () => {
         I try to learn constantly and I've enjoyed working with these so far
       </p>
       <ul>
-        <div className="grid grid-cols-6 text-center">
+        <div className="grid grid-cols-6">
           {frameworks.map((item) => (
-            <li key={item.name} className="">
+            <li key={item.name} className="border w-28 mx-auto">
               {item.name}
             </li>
           ))}
