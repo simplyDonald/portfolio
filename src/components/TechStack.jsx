45d4c2d8891b7777887a090fa17b136603fbd58b
simplyDonald <donaldabuah@gmail.com>
2022-04-02 22:00:10 -0400

    add responsive techstack component

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -10,13 +10,13 @@ const TechStack = () => {
   }, []);
 
   return (
-    <div id="tech" className="h-full mt-5 border border-purple-400">
+    <div id="tech" className="h-full mt-5">
       <h2 className="stroke text-3xl mt-8 ml-8">Some tools in my toolbox</h2>
       <p className="font-body mt-2 text-center text-[#64FFDA]">
         I try to learn constantly and I've enjoyed working with these so far
       </p>
       <ul>
-        <div className=" grid grid-cols-2 text-center mt-8 justify-items-center lg:grid-cols-6">
+        <div className=" grid grid-cols-3 text-center mt-8 justify-items-center lg:grid-cols-6">
           {frameworks.map((item) => (
             <li key={item.name} className="fofo hover:filter-none w-10 m-8">
               {item.name}
