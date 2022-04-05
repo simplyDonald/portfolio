c65f28ee69b8024ada982a0113e8788a5fbc7cc8
simplyDonald <donaldabuah@gmail.com>
2022-04-04 23:28:32 -0400

    remove hover color effect for social icons

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,19 +4,14 @@ import { socials } from "techStack";
 const Socials = () => {
  
   return (
-    <div className=" hidden fixed bottom-0 lg:block left-4  text-[#8892B0]">
+    <div className=" hidden fixed bottom-0 lg:block left-4  text-[#8892B0] ">
       {socials.map((item) => {
         return (
           <li
             key={item.name}
-            className="invert w-5 m-1 list-none inline-block "
+            className="invert w-5 m-1 list-none inline-block"
           >
-            <a
-              href={item.link}
-              rel="noreferrer"
-              target="_blank"
-              className="hover:text-[#64FFDA]"
-            >
+            <a href={item.link} rel="noreferrer" target="_blank" className="">
               <img src={item.img} alt="socials icon" className="" />
             </a>
           </li>
