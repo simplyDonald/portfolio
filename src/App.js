9732c1eb2de7adab55f9fc16b49b33c276a3a1be
simplyDonald <donaldabuah@gmail.com>
2022-05-12 16:00:10 -0400

    track screenWidth with useEffect

--- src/App.js
+++ src/App.js
@@ -11,13 +11,34 @@ import Footer from 'components/Footer';
 
 
 function App() {
-  const [isOpen, setOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const toggler = () => setOpen(!isOpen);
 
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth);
+    if((screenWidth > 1023) && (isOpen === true)){
+      toggler();
+    }
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [isOpen,screenWidth])
+
+  
+
+
+
   return (
     <div className={isOpen ? "h-full relative overflow-hidden" : "h-full relative"}>
-      {isOpen && (<div className="fixed text-black font-extrabold mt-0 h-full w-full bg-white z-10">
+      {(isOpen) && (<div className="fixed text-black font-extrabold mt-0 h-full w-full bg-[#6EBD9E] z-10">
         hello
       </div>)}                                        
       <Hero toggle={toggler} />
