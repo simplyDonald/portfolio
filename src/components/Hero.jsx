e4b8d2e22d0140d0e9e5a0c6ecbc657550b5c661
simplyDonald <donaldabuah@gmail.com>
2022-05-21 02:23:16 -0400

    add animation lib

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
@@ -10,14 +11,18 @@ function Hero() {
           Donald
           <span className="text-[#FFD700] y pl-0 font-sans"> Abuah.</span>{" "}
         </p>
-        <p className="text-white text-3xl pl-2 font-body">
-          <span className="italic">FullStack</span> <span className="font-semibold"> Software Developer</span> 
-        </p>
+        <motion.p
+          initial={{ x: -1000 }}
+          animate={{ x: 0 }}
+          className="text-white text-3xl pl-2 font-body"
+        >
+          <span className="italic">FullStack</span>{" "}
+          <span className="font-semibold"> Software Developer</span>
+        </motion.p>
         <p className="text-[#d8f3dc] text-base pl-2 leading-6 max-w-lg mt-3 font-body">
           I’m a software developer specializing in creating easy to use web
           experiences with engaging and appealing design.
         </p>
-      
       </div>
     </div>
   );
