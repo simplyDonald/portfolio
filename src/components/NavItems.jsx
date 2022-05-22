ce1159e64760a5a3e3d5a51331e452f3ffd78524
simplyDonald <donaldabuah@gmail.com>
2022-05-22 10:48:34 -0400

    add dynamic font-size f9or rersumelink

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -10,6 +10,12 @@ const NavItems = ({layout}) => {
     "text-4xl block py-4": `${layout}` === "sideMenu"               
   });
 
+
+  const resumeClass = classNames(
+    "border border-[#64FFDA]   hover:bg-[#0c5745] text-[#64FFDA] inline-block px-3 py-2 cursor-pointer rounded transition ease-in duration-300",
+    { "text-2xl": `${layout}` === "sideMenu" }
+  );
+
   const itemVariants = {
     initial: {
       opacity: 0,
@@ -44,7 +50,7 @@ const NavItems = ({layout}) => {
           <a href={item.link}>{item.name}</a>
         </motion.li>
       ))}
-      <motion.li variants={resumeVariants} className="border border-[#64FFDA]   hover:bg-[#0c5745] text-[#64FFDA] inline-block px-3 py-2 cursor-pointer rounded transition ease-in duration-300">
+      <motion.li variants={resumeVariants} className={resumeClass}>
         <a
           rel="noreferrer"
           href="https://resume.io/r/dAzgD3MkP"
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import Socials from "./Socials";
-import { items } from "techStack";
+import NavItems from "./NavItems";
 
 
 const SideMenu = ({track, toggle}) => {
@@ -24,21 +24,6 @@ const SideMenu = ({track, toggle}) => {
   };
 
 
-  const itemVariants = {
-    initial: {
-      opacity: 0,
-      y: -50
-    },
-    animate: {
-      opacity: 1,
-      y: 0
-      
-    }
-  };
-
-
-  
-
   return (
     <motion.div
       className="fixed text-white font-extrabold grid grid-cols-3 grid-flow-col inset-0  z-10"
@@ -59,35 +44,9 @@ const SideMenu = ({track, toggle}) => {
         <div
           className="flex flex-col justify-center items-center h-full pt-36"
         >
-          <ul 
-            className="mb-8"
-          >
-            {items.map((item) => (
-              <motion.li
-                key={item.name}
-                className="hover:text-[#64FFDA] text-4xl text-white block px-3 py-4 cursor-pointer"
-                variants={itemVariants}
-                onClick={toggle}
-              >
-                <a 
-                  href={item.link}
-                >
-                  {item.name}
-                </a>
-              </motion.li>
-            ))}
-            <li
-              className="border border-[#64FFDA] ml-5   hover:bg-[#0c5745] text-[#64FFDA] text-2xl inline-block px-3 py-2 cursor-pointer rounded transition ease-in duration-300"
-            >
-              <a
-                rel="noreferrer"
-                href="https://resume.io/r/dAzgD3MkP"
-                target="_blank"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
+          <div className="mb-8">
+            <NavItems layout="sideMenu" />
+          </div>
 
           {/* Social Icons below */}
           <div className="pr-12">
