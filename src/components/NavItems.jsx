08158f4ce7a7c6949c218aa3456d076702a2eda0
simplyDonald <donaldabuah@gmail.com>
2022-05-22 10:50:50 -0400

    fix margin on resume link

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -13,7 +13,7 @@ const NavItems = ({layout}) => {
 
   const resumeClass = classNames(
     "border border-[#64FFDA]   hover:bg-[#0c5745] text-[#64FFDA] inline-block px-3 py-2 cursor-pointer rounded transition ease-in duration-300",
-    { "text-2xl": `${layout}` === "sideMenu" }
+    { "text-2xl ml-4": `${layout}` === "sideMenu" }
   );
 
   const itemVariants = {
