d46cdf925c715b29ed081579be22b0e399f12bd6
simplyDonald <donaldabuah@gmail.com>
2022-05-22 12:12:53 -0400

    add hperlink to diamond logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -27,11 +27,17 @@ const Nav = ({track, toggle}) => {
   return (
     <nav className=" backdrop-blur-sm fixed top-0 left-0 flex justify-between  py-4  px-5 z-10 items-center w-full transition-all ease-in-out duration-500 ">
       <div className="">
-        <img
-          src="hero-logo.png"
-          alt="diamond logo"
-          className={track ? "hidden" : " w-20 h-20 p-2 invert rotate-12"}
-        />
+        <a
+          rel="noreferrer"
+          href="https://donaldabuah.com"
+          
+        >
+          <img
+            src="hero-logo.png"
+            alt="diamond logo"
+            className={track ? "hidden" : " w-20 h-20 p-2 invert rotate-12"}
+          />
+        </a>
       </div>
 
       <div
