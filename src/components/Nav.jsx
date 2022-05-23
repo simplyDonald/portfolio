70c44ec1296d43b4493af75fcbd65f4b94108724
simplyDonald <donaldabuah@gmail.com>
2022-05-23 13:32:15 -0400

    change hamburger to 2 slits

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { Spin as Hamburger } from "hamburger-react";
+import { Slant as Hamburger } from "hamburger-react";
 import { motion } from "framer-motion";
 import NavItems from "./NavItems";                                       
 
