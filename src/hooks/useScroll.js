cb10bd1deabc3f7bdf2e29ba3e5c1cfdba24d621
simplyDonald <donaldabuah@gmail.com>
2022-05-22 21:08:02 -0400

    animate contact component

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,39 @@
+import  { useEffect } from 'react';
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+
+const useScroll = () => {
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    // Scroll Animation detection
+
+    if (inView) {
+      controls.start("visible");                                                  
+    }
+  }, [controls, inView]);
+
+  const itemVariants = {
+      hidden: {
+        opacity: 0,
+        // y: 50,
+      },
+      visible: {
+        opacity: 1,
+        // y: 0,
+        transition: {
+          type: "tween",
+          duration: 1,
+          delay: .3
+        },
+      },
+    };
+
+  return {
+    ref, controls, itemVariants
+  };
+}
+
+export default useScroll;
