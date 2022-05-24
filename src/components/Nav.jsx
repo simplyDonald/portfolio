9e619d393deaa28c9804179e7d7be68493a84290
simplyDonald <donaldabuah@gmail.com>
2022-05-24 14:15:29 -0400

    bugfix for hamburger

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -38,12 +38,12 @@ const Nav = ({track, toggle}) => {
 
   return (
     <nav className=" backdrop-blur-sm fixed top-0 left-0 flex justify-between  py-4  px-5 z-10 items-center w-full transition-all ease-in-out duration-500 ">
-      <div className={track ? "hidden" : " w-16 p-2 invert rotate-12"}>
+      <div className="">
         <a rel="noreferrer" href="https://donaldabuah.com">
           <img
             src="compass.svg"
             alt="compass logo"
-            className=""
+            className={track ? "hidden" : " w-16 p-2 invert rotate-12"}
           />
         </a>
       </div>
