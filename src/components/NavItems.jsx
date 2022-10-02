9abf63e87c722690fbee578dec43bddb42dcf21e
simplyDonald <donaldabuah@gmail.com>
2022-10-01 21:09:14 -0400

    edit margin left for resume button

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -21,7 +21,7 @@ const NavItems = ({track,toggle,layout}) => {
 
   const resumeClass = classNames(
     "border border-[#64FFDA]   hover:bg-[#0c5745] text-[#64FFDA] inline-block px-3 py-2 cursor-pointer rounded transition ease-in duration-300",
-    { "text-2xl ml-4": `${layout}` === "sideMenu" }
+    { "text-2xl ": `${layout}` === "sideMenu" }
   );
 
   const itemVariants = {
@@ -36,7 +36,7 @@ const NavItems = ({track,toggle,layout}) => {
         ease: "easeIn"
       }
     },
-  };
+  };                                                                                                                                                                                                      
 
 
   return (
