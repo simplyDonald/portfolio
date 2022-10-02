53d8ddfcc8ebc7ca0a5b849e1908cd2f1efab961
simplyDonald <donaldabuah@gmail.com>
2022-10-02 19:45:16 -0400

    hide horizontal scrollbar

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
 
   return (
-    <div className="h-full relative">
+    <div className="h-full relative scrollbar overflow-x-hidden">
       <AnimatePresence >
         {(isOpen) && <SideMenu track={isOpen} toggle={toggler}/>}   
 
